98fffa264a3660e2a2a0ba68aa07170dd86124af
NaikHarshal <naikharshal9211@gmail.com>
2024-12-07 14:32:59 +0530

    test script 3 clean

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print('the breakpoint is successful')
+    print(f'Hi, {name} , welcome, You have successfully created git repo')
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Harshal')
-    print_hi('vrushi')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
