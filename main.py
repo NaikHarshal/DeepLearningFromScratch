9e9587fc98e9a328123b52c7058a7c79cdb1188d
NaikHarshal <naikharshal9211@gmail.com>
2024-12-07 20:35:52 +0530

    Implemented perceptron, the output is reversed i.e. getting 0 in place of 1 and 1 in place of 0. The OR gate input output is tested.
    in next iteration, lots of points will be used.

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ def print_hi(name):
     print(f'Hi, {name} , welcome, You have successfully created git repo')
 
 if __name__ == '__main__':
-    print_hi('Harshal')
+    print_hi('Harshal'
+
+
 
---
+++ perceptron.py
@@ -0,0 +1,75 @@
+import math
+
+import numpy as np
+
+def perceptron(X, w, b):
+  z = np.dot(X, w) + b
+  # return 1 if z > 0 else 0
+  return max(0, z) , 1 if z > 0 else 0
+
+
+train_data = np.array([[0,0],[0,1],[1,0],[1,1]])
+val_data = np.array([0,1,1,1])
+import random
+w = np.array([random.random(),-random.random()])  # Weights initialization
+w = np.array([0.1, -0.6])  # Weights initialization manually
+
+
+b = 0.5 # Bias
+learning_rate = 0.3
+decay_factor = 0.9
+
+
+
+for i,j in zip(train_data,val_data):
+  print(w,b)
+
+  output,ans = perceptron(i, w, b)
+  print(i,j,output)
+  # error = math.sqrt((output - j)**2)
+  error= output - j
+  print(error)
+
+  w += learning_rate * error * i
+  b += learning_rate * error
+  learning_rate *=decay_factor
+print(-w[0]/w[1] ,-b/w[1])
+
+print('training Complete')
+
+
+
+
+## create scatterplot
+import matplotlib.pyplot as plt
+# Extract x and y coordinates
+x = train_data[:, 0]
+y = train_data[:, 1]
+
+# Plot the points
+plt.scatter(x, y)
+# Define the line's parameters (adjust as needed)
+m = -w[0]/w[1]  # Slope
+c = -b/w[1]  # Intercept
+
+# Generate x values for the line
+x_line = np.linspace(min(x), max(x), 100)
+# Calculate y values for the line
+y_line = m * x_line + c
+# Plot the line
+plt.plot(x_line, y_line, color='red', label='y = 2x + 1')
+
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Scatter Plot of NumPy Array')
+plt.grid(True)
+plt.show()
+
+##manually verify the ourput
+while True:
+  X0 = int(input("insert X0 : "))
+  X1 = int(input("insert X1 : "))
+  Xin = np.array([X0,X1])
+  output,ans = perceptron(Xin, w, b)
+
+  print(ans)
